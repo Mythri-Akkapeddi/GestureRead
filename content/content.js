940d5038524b1b15efff59f124c499e96f7f3802
Mythri-Akkapeddi <My3Ace496@gmail.com>
2026-08-08 17:40:01 +0530

    Add content script

---
+++ content/content.js
@@ -0,0 +1,3 @@
+//Empty shell for now to confirm that the content script is injected into every page.
+// Will implement real gesture overlay later.
+console.log("[GestureRead] content script injected on", window.location.href);
