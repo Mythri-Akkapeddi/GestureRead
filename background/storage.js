3497297851a3fce9f6a7f626f69ed981b288743f
Mythri-Akkapeddi <My3Ace496@gmail.com>
2026-08-09 18:35:39 +0530

    Add storage message handler

---
+++ background/storage.js
@@ -0,0 +1,34 @@
+// Storage message handler.
+// All chrome.storage.local access is owned by this module (via storageHelpers.js).
+// background.js receives the message and calls handleStorageMessage() instead of touching storage itself, keeping background.js focused on routing.
+
+import * as storageHelpers from "../utils/storageHelpers.js";
+import { MESSAGE_TYPES } from "../utils/constants.js";
+
+export async function handleStorageMessage(message) {
+  switch (message.type) {
+    case MESSAGE_TYPES.GET_CALIBRATION:
+      return { ok: true, data: await storageHelpers.getCalibration() };
+
+    case MESSAGE_TYPES.SAVE_CALIBRATION:
+      return { ok: await storageHelpers.saveCalibration(message.payload) };
+
+    case MESSAGE_TYPES.GET_THRESHOLDS:
+      return { ok: true, data: await storageHelpers.getThresholds() };
+
+    case MESSAGE_TYPES.SAVE_THRESHOLDS:
+      return { ok: await storageHelpers.saveThresholds(message.payload) };
+
+    case MESSAGE_TYPES.APPEND_GESTURE_LOG:
+      return { ok: true, data: await storageHelpers.appendGestureLog(message.payload) };
+
+    case MESSAGE_TYPES.GET_GESTURE_LOGS:
+      return { ok: true, data: await storageHelpers.getGestureLogs(message.payload?.sessionId) };
+
+    case MESSAGE_TYPES.CLEAR_LOGS:
+      return { ok: await storageHelpers.clearLogs() };
+
+    default:
+      return { ok: false, error: `Unknown storage message type: ${message.type}` };
+  }
+}
