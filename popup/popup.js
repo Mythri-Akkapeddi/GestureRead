393e447aab0c1fe357b07b101d2c0c85f2bbfd58
Mythri-Akkapeddi <My3Ace496@gmail.com>
2026-09-08 09:39:01 +0530

    Add calibration button handler

--- popup/popup.js
+++ popup/popup.js
@@ -4,3 +4,16 @@ console.log("[GestureRead] popup opened");
 chrome.runtime.sendMessage({ type: "PING_FROM_POPUP" }, (response) => {
   console.log("[GestureRead] background responded:", response);
 });
+
+document.getElementById("gr-calibrate-btn").addEventListener("click", async () => {
+  const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const url = chrome.runtime.getURL(`settings/settings.html?tabId=${activeTab.id}`);
+  chrome.windows.create({
+    url,
+    type: "popup",
+    width: 480,
+    height: 640,
+    left: 40,
+    top: 40,
+  });
+});
