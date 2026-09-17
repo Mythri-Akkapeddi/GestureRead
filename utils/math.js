59b40263d4a7eb9aec1d1fa3770e8e687bc18669
Mythri-Akkapeddi <My3Ace496@gmail.com>
2026-09-17 15:24:22 +0530

    Add normalization to keep adaptive multipliers within range

--- utils/math.js
+++ utils/math.js
@@ -20,3 +20,8 @@ function rollingMean(values) {
   if (!values || values.length === 0) return 0;
   return values.reduce((sum, v) => sum + v, 0) / values.length;
 }
+
+function normalizeToRange(value, min, max) {
+  // Clamps a value into [min, max]. Used to keep adaptive multipliers within safe bounds.
+  return Math.min(Math.max(value, min), max);
+}
